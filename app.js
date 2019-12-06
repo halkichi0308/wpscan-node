bf4f0dc9bd82e7d312d510f2be86f05428f305e2
halkichi0308 <m.kouda0308@gmail.com>
2019-12-07 03:30:13 +0900

    Fix CORS && app.options

--- app.js
+++ app.js
@@ -5,39 +5,24 @@ const app = express()
 
 const port = 3000
 app.get('/ack', (req, res) => {
-  if(req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST')
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
-    res.send(200)
-  }
   if(req.method === 'GET'){
     res.send(200, 'instance ok!')
   }
 })
+app.options('*',(req, res)=>{
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.send(200)
+}) 
 
 app.get('/scan', (req, res) => {
-  if(req.method === 'GET'){
-  let template_html = fs.readFileSync('./html/template.html').toString()
-
-  console.log(req.query.url)
-  let exec_query = `docker run -t wpscanteam/wpscan --url ${req.query.url}`
+  //let template_html = fs.readFileSync('./html/template.html').toString()
+  let exec_query = `docker run -t wpscanteam/wpscan --url ${req.query.url} -f json`
   console.log(exec_query)
   let scan_result = exec(exec_query).toString()
-  let dumped = scan_result.replace(/\[32m(.*?)$/mg,(match)=>{
-    return `<h3>問題点:${match}</h3>`
-  }).replace(/\[32m/g, '')
-  let result = template_html.replace(/@@report@@/mg,dumped)
-  res.header({'Content-type': 'text/html'})
-  
+  res.header({'Content-type': 'application/json'})
   res.send(200, result)
-  }
-  if(req.method === 'OPTIONS'){
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST')
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
-    res.send(200)
-  }
 })
 
 app.listen(port)
