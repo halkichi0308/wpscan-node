ab0704dd23f45627354a1201f8ad98c276016b0a
halkichi0308 <m.kouda0308@gmail.com>
2019-12-07 02:47:09 +0900

    Fix ack End Point

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express()
 
 const port = 3000
 app.get('/ack', (req, res) => {
-  res.send(200, 'instance ok!')
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.send(200)
+  }
+  if(req.method === 'GET'){
+    res.send(200, 'instance ok!')
+  }
 })
 
 app.get('/scan', (req, res) => {
@@ -18,7 +26,7 @@ app.get('/scan', (req, res) => {
   let scan_result = exec(exec_query).toString()
   let dumped = scan_result.replace(/\[32m(.*?)$/mg,(match)=>{
     return `<h3>問題点:${match}</h3>`
-  }).replace(/\[32m/, '')
+  }).replace(/\[32m/g, '')
   let result = template_html.replace(/@@report@@/mg,dumped)
   res.header({'Content-type': 'text/html'})
   
